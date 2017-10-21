2f55864d6fd9bdf6d47bcfa768a3df2efeef8711
Ingo <demanufactures@web.de>
2017-10-21 22:30:20 +0200

    tests

--- src/cMain.py
+++ src/cMain.py
@@ -12,24 +12,51 @@ booRaw=True
 
 if ser.isOpen():
     print("Serial is open")
-    ser.write("f 0 5658")
+
+    ser.write("s\n")
+    ser.flush()
+    
+    print("RX 0 setzen")
+    ser.write("f 0 5880\n")
+    ser.flush()
+    booWarte=True
+    while booWarte:
+        data=ser.readline()
+        
+        if "ok" in data:
+            booWarte=False
     #if (ser.inWaiting() > 0):
     #    data = ser.read(ser.inWaiting())
     #    print(data)
 
-    ser.write("f 1 5732")
+    print("RX 1 setzen")
+    ser.write("f 1 5658\n")
+    #ser.flush()
     #if (ser.inWaiting() > 0):
     #    data = ser.read(ser.inWaiting())
     #    print(data)
 
-    ser.write("f 2 5880")
+    ser.write("f 2 5658\n")
+    #ser.flush()
     #if (ser.inWaiting() > 0):
     #    data = ser.read(ser.inWaiting())
     #    print(data)
 
+    print("Power Off Now")
+    time.sleep(5)
+    ser.write("n\n")
+    ser.flush()
+    
+    print("Power On Now")
+    time.sleep(5)
+    ser.write("m\n")
+    time.sleep(2)
+    ser.write("s")
     while True:
         time.sleep(5)
         ser.write("?")
+        ser.flush()
+        print("Status?")
 
 else:
     print("Serial is not open")
--- src/serial_read.py
+++ src/serial_read.py
@@ -2,12 +2,25 @@ import serial
 import time
 
 strSerPort = "/dev/ttyS1"
-
+message=""
+eol='\x0d'
+leneol=len(eol)
 
 ser= serial.Serial(port=strSerPort, baudrate=250000)
 #ser.open()
 
 while True:
 
-    x = ser.readline()
-    print(x)
+    if ser.inWaiting():
+        ser_data = ser.readline()
+        if ser_data !="":
+            message=ser_data
+            #if "\x0d" in message:
+                #print(":::"+message+":::")
+                #message=message + ser_data
+            if message[0] !="r":
+                print(":::"+message+":::")
+                message=""
+            #else:
+                #pass
+                #message= message + ser_data
