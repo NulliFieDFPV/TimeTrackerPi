84879e8c9991e6f93ce46d73e1e419a5c4f45c0a
Ingo (NulliFieD) <ingo_o@loop.de>
2017-10-21 20:01:09 +0200

    main

--- src/cMain.py
+++ src/cMain.py
@@ -7,15 +7,21 @@ ser= serial.Serial(port=strSerPort, baudrate=250000)
 ser.close()
 ser.open()
 
-while True:
-    if ser.isOpen():
+ser.write("f 0 5658")
+ser.write("f 0 5732")
+ser.write("f 0 5880")
+
+if ser.isOpen():
+    while True:
+
         print("Serial is open")
 
         if (ser.inWaiting()>0):
-            data=ser.read(ser.inWaiting()).decode()
+            data=ser.read(ser.inWaiting())
             print(data)
-    else:
-        print("Serial is not open")
 
-    time.sleep((2))
 
+        time.sleep((2))
+
+else:
+    print("Serial is not open")
