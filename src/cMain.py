0880464ad902aa19437a81b2dd194e4c8cecef39
Ingo <demanufactures@web.de>
2017-10-22 00:24:26 +0200

    bla

--- src/cMain.py
+++ src/cMain.py
@@ -13,95 +13,38 @@ booRaw=True
 if ser.isOpen():
     print("Serial is open")
 
-    ser.write("s\n")
-    ser.flush()
-    booWarte = True
-    while booWarte:
-        data = ser.readline()
-
-        if "ok" in data:
-            booWarte = False
-
+    print("set calib")
+    #ser.write("s\n")
+    ser.write("r\n")
+    #ser.flush()
     print("RX 0 setzen")
-    ser.write("f 0 5880\n")
-    ser.flush()
-    booWarte=True
-    while booWarte:
-        data=ser.readline()
-        
-        if "ok" in data:
-            booWarte=False
+    ser.write("f 0 5658\n")
+    #ser.flush()
+ 
     #if (ser.inWaiting() > 0):
     #    data = ser.read(ser.inWaiting())
     #    print(data)
 
     print("RX 1 setzen")
-    ser.write("f 1 5658\n")
-    booWarte = True
-    while booWarte:
-        data = ser.readline()
-
-        if "ok" in data:
-            booWarte = False
-
+    ser.write("f 1 5880\n")
+ 
     #ser.flush()
     #if (ser.inWaiting() > 0):
     #    data = ser.read(ser.inWaiting())
     #    print(data)
-
+    print("RX 2 setzen")
     ser.write("f 2 5658\n")
     ser.flush()
-    booWarte = True
-    while booWarte:
-        data = ser.readline()
-
-        if "ok" in data:
-            booWarte = False
-    #if (ser.inWaiting() > 0):
+     #if (ser.inWaiting() > 0):
     #    data = ser.read(ser.inWaiting())
     #    print(data)
-
-    print("Power Off Now")
-    time.sleep(5)
-    ser.write("n\n")
-    ser.flush()
-    booWarte = True
-    while booWarte:
-        data = ser.readline()
-
-        if "ok" in data:
-            booWarte = False
-
-    print("Power On Now")
-    time.sleep(5)
-    ser.write("m\n")
-    booWarte = True
-    while booWarte:
-        data = ser.readline()
-
-        if "ok" in data:
-            booWarte = False
-
-    time.sleep(2)
-    ser.write("s")
-    booWarte = True
-    while booWarte:
-        data = ser.readline()
-
-        if "ok" in data:
-            booWarte = False
 
     while True:
         time.sleep(5)
         ser.write("?")
         ser.flush()
-        booWarte = True
-        while booWarte:
-            data = ser.readline()
-
-            if "ok" in data:
-                booWarte = False
-        print("Status?")
+        #data = ser.readline()
+	#print data
 
 else:
     print("Serial is not open")
