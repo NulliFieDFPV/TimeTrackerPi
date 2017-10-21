475f9c24c73824f9f4f6f66e990f20721a934cdc
Ingo (NulliFieD) <ingo_o@loop.de>
2017-10-22 00:18:39 +0200

    main

--- src/cMain.py
+++ src/cMain.py
@@ -1,106 +1,38 @@
 import serial
 import time
+import io
 
 strSerPort="/dev/ttyS1"
+intBaudRate=250000
 
-ser= serial.Serial(port=strSerPort, baudrate=250000)
+ser= serial.Serial()
+ser.baudrate=intBaudRate
+ser.port=strSerPort
+
 ser.close()
 ser.open()
 
-time.sleep(3)
+sio= io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+
 booRaw=True
 
-if ser.isOpen():
+if sio.isOpen():
     print("Serial is open")
 
-    ser.write("s\n")
-    ser.flush()
-    booWarte = True
-    while booWarte:
-        data = ser.readline()
-
-        if "ok" in data:
-            booWarte = False
+    sio.write(u's')
 
     print("RX 0 setzen")
-    ser.write("f 0 5880\n")
-    ser.flush()
-    booWarte=True
-    while booWarte:
-        data=ser.readline()
-        
-        if "ok" in data:
-            booWarte=False
-    #if (ser.inWaiting() > 0):
-    #    data = ser.read(ser.inWaiting())
-    #    print(data)
+    sio.write(u'f 0 5880')
 
     print("RX 1 setzen")
-    ser.write("f 1 5658\n")
-    booWarte = True
-    while booWarte:
-        data = ser.readline()
+    sio.write(u'f 1 5880')
 
-        if "ok" in data:
-            booWarte = False
-
-    #ser.flush()
-    #if (ser.inWaiting() > 0):
-    #    data = ser.read(ser.inWaiting())
-    #    print(data)
-
-    ser.write("f 2 5658\n")
-    ser.flush()
-    booWarte = True
-    while booWarte:
-        data = ser.readline()
-
-        if "ok" in data:
-            booWarte = False
-    #if (ser.inWaiting() > 0):
-    #    data = ser.read(ser.inWaiting())
-    #    print(data)
-
-    print("Power Off Now")
-    time.sleep(5)
-    ser.write("n\n")
-    ser.flush()
-    booWarte = True
-    while booWarte:
-        data = ser.readline()
-
-        if "ok" in data:
-            booWarte = False
-
-    print("Power On Now")
-    time.sleep(5)
-    ser.write("m\n")
-    booWarte = True
-    while booWarte:
-        data = ser.readline()
-
-        if "ok" in data:
-            booWarte = False
-
-    time.sleep(2)
-    ser.write("s")
-    booWarte = True
-    while booWarte:
-        data = ser.readline()
-
-        if "ok" in data:
-            booWarte = False
+    print("RX 2 setzen")
+    sio.write(u'f 2 5880')
 
     while True:
         time.sleep(5)
-        ser.write("?")
-        ser.flush()
-        booWarte = True
-        while booWarte:
-            data = ser.readline()
-
-            if "ok" in data:
-                booWarte = False
+        sio.write(u"?")
         print("Status?")
 
 else:
--- src/serial_read.py
+++ src/serial_read.py
@@ -1,17 +1,28 @@
 import serial
 import time
+import io
 
 strSerPort = "/dev/ttyS1"
+intBaudRate=250000
+
+ser= serial.Serial()
+ser.baudrate=intBaudRate
+ser.port=strSerPort
+
+ser.close()
+ser.open()
+
 message=""
 eol='\x0d'
 leneol=len(eol)
 
 ser= serial.Serial(port=strSerPort, baudrate=250000)
+sio= io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 #ser.open()
 
 while True:
 
-    if ser.inWaiting():
+    if sio.inWaiting():
         ser_data = ser.readline()
         if ser_data !="":
             message=ser_data
