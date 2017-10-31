1693417c80d25103abee704dcf4568ffdb2bebd8
Ingo <demanufactures@web.de>
2017-10-31 14:46:47 +0100

    rumtesterei pwm und rssi

--- src/serial_read.py
+++ src/serial_read.py
@@ -1,10 +1,52 @@
+from __future__ import division
 import serial
 import time
 import io
+import Adafruit_PCA9685 as ADAdriver
+
+
+def set_servo_pulse(channel, pulse):
+    pulse_length = 10000000
+    pulse_length //= 60
+    print("{0}us per period".format(pulse_length))
+    pulse_length //= 4096
+    print("{0}us per bit".format(pulse_length))
+    pulse *=1000
+    pulse //= pulse_length
+
+    pwm.set_pwm(channel,0,pulse)
+
 
 strSerPort = "/dev/ttyS1"
 intBaudRate=250000
-intAvg=500
+intAvg=50
+booCalib=False
+
+pwm=ADAdriver.PCA9685(address=0x40, busnum=0)
+pwm.set_pwm_freq(60)
+
+
+servo_min=150
+servo_max=600
+
+print("Move it")
+#set_servo_pulse(0,150)
+
+
+
+print("to the min")
+for i in range(15):
+    pwm.set_pwm(i,0,servo_min)
+    time.sleep(0.2)
+
+time.sleep(1)
+
+print("to the max")
+for i in range(15):
+    pwm.set_pwm(i,0,servo_max)
+    time.sleep(0.2)
+
+time.sleep(1)
 
 ser= serial.Serial()
 ser.baudrate=intBaudRate
@@ -16,6 +58,29 @@ if ser.isOpen()==False:
 
 intSum=0
 rssisSum={}
+ser.write("s\n")
+
+ser.write("f 0 5658\n")
+ser.write("f 1 5658\n")
+ser.write("f 2 5658\n")
+
+ser.write("?\n")
+
+if booCalib:
+
+    print("calib")
+    time.sleep(3)
+
+    ser.write("n\n")
+
+    time.sleep(2)
+    print("max")
+    time.sleep(5)
+
+    ser.write("m\n")
+
+minRssi=260.0
+maxRssi=0.0
 
 while True:
     
@@ -35,9 +100,30 @@ while True:
                 if intSum>=intAvg:
                     intSum=0
                     for channel, rssi in rssisSum.iteritems():
-                      
-                        print(str(channel) + " " + str((round(rssi/intAvg))))
-                    rssisSum={}
+                        rssi_raw=round(rssi/intAvg)
+                        floRssi=rssi_raw
+
+                        if floRssi>10: 
+                            if floRssi<minRssi:
+                                minRssi=floRssi
+                            if floRssi>maxRssi and floRssi<255:
+                                maxRssi=floRssi
+          
+                        pwmValue=servo_min
+
+                        floDiff=floRssi/(maxRssi+minRssi)
+                        if floDiff<0:
+                            floDiff=0
+                        if floDiff>1:
+                            floDiff=1
+
+                        pwmValue=servo_max*floDiff 
+                        print("set: " + str(servo_min) + " > " + str(pwmValue) + " < " + str(servo_max))
+                        pwm.set_pwm(channel, 0, int(pwmValue))
+
+                        print(str(channel) + " " + str(rssi_raw) + " (" + str(minRssi) + "-" + str(maxRssi)+" ["+str(floDiff) +"])")
+                    for channel, rssi in rssisSum.iteritems():
+                        rssisSum[channel]=0
 
                 for channel in range(len(rssis)):
                     intRealChannel=channel+1
